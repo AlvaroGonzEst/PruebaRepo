606bb7efe84abbe7bab2873fd8ffa19bb6ee3ce6
Alvaro <daw202610@estudiantes.salesianasnsp.es>
2025-04-24 12:28:09 +0200

    Añadido Script BBDD

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PruebaRepo
 Repo de prueba para la práctica de Entornos de desarrollo
+
+Atención! Añadida interacción con BBDD, el script SQL se encuentra en la parte inicial del proyecto.
---
+++ Taste.sql
@@ -0,0 +1,46 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1deb5ubuntu1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost:3306
+-- Tiempo de generación: 24-04-2025 a las 12:22:26
+-- Versión del servidor: 8.0.41-0ubuntu0.22.04.1
+-- Versión de PHP: 8.1.2-1ubuntu2.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `Taste`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `PLAYER`
+--
+
+CREATE TABLE `PLAYER` (
+  `NAME` varchar(20) NOT NULL,
+  `SOUL` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Volcado de datos para la tabla `PLAYER`
+--
+
+INSERT INTO `PLAYER` (`NAME`, `SOUL`) VALUES
+('Aiden', 'Valor'),
+('Andrei', 'Perseverancia');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
